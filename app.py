2f5facb09da3ff6e077ae6da27ce1fca539e4259
lingyang888 <lingyangpsu@gmail.com>
2020-09-16 13:01:20 -0400

    bug fix

--- app.py
+++ app.py
@@ -32,43 +32,55 @@ def graph():
 
     url1 = 'https://www.google.com/search?tbm=fin&q=' + symbol
 
-
     api_key='XPHT5FFL8U5Q53PN'
     function = 'TIME_SERIES_DAILY_ADJUSTED'
 
     url='https://www.alphavantage.co/query?function=' + function + '&symbol=' + symbol + '&apikey=' + api_key
-
-    stock = requests.get(url).json()
-    df = pd.DataFrame(stock['Time Series (Daily)'])
-    df = df.T
-
-    a = list(df.index)
-    end=a.index('2020-05-01')+1
-    start=a.index('2020-06-01')
-    x = a[start:end]
-
-    x = pd.to_datetime(x)
-
-    op = df['1. open'][start:end]
-    op = op.astype(float)
-
-    cl = df['4. close'][start:end]
-    cl = cl.astype(float)
-
-    adj_cl = df['5. adjusted close'][start:end]
-    adj_cl = adj_cl.astype(float)
 
     p1 = figure(x_axis_type='datetime', title = 'Stock 2020: '+symbol,
                 x_axis_label = 'date', y_axis_label = 'price',
                 plot_width=600, plot_height=600)
     p1.title.text_font_size = '15pt'
     p1.title.align = 'center'
-    if app.vars['open']:
-        p1.line(x,op,legend_label="open",line_color='blue')
-    if app.vars['close']:
-        p1.line(x,cl,legend_label='close',line_color='red')
-    if app.vars['adj_close']:
-        p1.line(x,adj_cl,legend_label='adj close',line_color='green')
+
+    try:
+        stock = requests.get(url).json()
+        df = pd.DataFrame(stock['Time Series (Daily)'])
+
+        df = df.T
+
+        a = list(df.index)
+        end=a.index('2020-05-01')+1
+        start=a.index('2020-06-01')
+        x = a[start:end]
+
+        x = pd.to_datetime(x)
+
+        op = df['1. open'][start:end]
+        op = op.astype(float)
+
+        cl = df['4. close'][start:end]
+        cl = cl.astype(float)
+
+        adj_cl = df['5. adjusted close'][start:end]
+        adj_cl = adj_cl.astype(float)
+
+        if app.vars['open']:
+            p1.line(x,op,legend_label="open",line_color='blue')
+        if app.vars['close']:
+            p1.line(x,cl,legend_label='close',line_color='red')
+        if app.vars['adj_close']:
+            p1.line(x,adj_cl,legend_label='adj close',line_color='green')
+
+    except:
+        x=[]
+        y=[]
+        if app.vars['open']:
+            p1.line(x,y,legend_label="open",line_color='blue')
+        if app.vars['close']:
+            p1.line(x,y,legend_label='close',line_color='red')
+        if app.vars['adj_close']:
+            p1.line(x,y,legend_label='adj close',line_color='green')
 
     # grab the static resources
     js_resources = INLINE.render_js()
