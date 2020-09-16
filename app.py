02adf250ca7ea9aaa6b8b8f0ee241a46cfe7be32
lingyang888 <lingyangpsu@gmail.com>
2020-09-15 21:54:29 -0400

    layout

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-@app.route('/index',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
