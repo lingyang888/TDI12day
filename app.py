e99554b433a7fe89a408111a7dca4ee3d8638ece
lingyang888 <lingyangpsu@gmail.com>
2020-09-15 21:18:07 -0400

    layout

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, redirect
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index',methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        return render_template('graph.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+#@app.route('/graph')
+#def graph():
+#    return render_template('graph.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    #app.run(port=33507)
+    app.run(debug=True)
